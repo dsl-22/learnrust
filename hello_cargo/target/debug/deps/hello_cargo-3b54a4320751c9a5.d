2ef8ec864683a13c3b56d14a1821e88badf8ec05
Zand <zand22@gmail.com>
2026-04-16 16:41:14 -0700

    Hello World! cargo project

---
+++ hello_cargo/target/debug/deps/hello_cargo-3b54a4320751c9a5.d
@@ -0,0 +1,5 @@
+/home/zand22/Scripts/Projects/Rust/LearnRust/hello_cargo/target/debug/deps/hello_cargo-3b54a4320751c9a5.d: src/main.rs
+
+/home/zand22/Scripts/Projects/Rust/LearnRust/hello_cargo/target/debug/deps/hello_cargo-3b54a4320751c9a5: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/debug/deps/hello_cargo-6bc71f01ad721c26.d
@@ -0,0 +1,5 @@
+/home/zand22/Scripts/Projects/Rust/LearnRust/hello_cargo/target/debug/deps/hello_cargo-6bc71f01ad721c26.d: src/main.rs
+
+/home/zand22/Scripts/Projects/Rust/LearnRust/hello_cargo/target/debug/deps/libhello_cargo-6bc71f01ad721c26.rmeta: src/main.rs
+
+src/main.rs:
